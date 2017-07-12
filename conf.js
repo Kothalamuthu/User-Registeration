cb8621aa174df819bef68ae86e470b03c7f13aa6
kothalamuthu <kothalamuthuvicky@gmail.com>
2017-07-12 14:54:26 +0530

    routes-update@kothalamuthu

--- conf.js
+++ conf.js
@@ -1,3 +1,14 @@
 /**
  * Created by w5rtc on 11/7/17.
  */
+var _=require("underscore");
+
+var conf = () ;
+
+var ENV= "development";
+
+var envconf = requires(".conf/conf-"+ENV+".js");
+
+conf=_.extent(envconf);
+
+module.exports=conf;
